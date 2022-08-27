88367a920454bcfbbb85c9c32e40a9e337429728
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-27 17:50:58 -0300

    Funcao createServer

---
+++ 1_NODE_BASICO/2_funcao_createServer/server.js
@@ -0,0 +1,20 @@
+//Carrega o módulo HTTP
+http = require('http')
+
+//Função de callback para o servidorHTTP
+var cb = function(req, res){
+    //define o cabeçalho (header) com o tipo da resposta
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+
+    //Mensagen de retorno
+    res.end("Hello World node callback")
+}
+
+//Servidor
+var server = http.createServer(cb)
+
+//Porta que o servidor vai escultar
+server.listen(3000)
+
+//Messagem ao iniciar o servidor
+console.log('Servidor iniciado na porta 3000!')
