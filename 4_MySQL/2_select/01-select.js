52ce8ddef7c83c8949eb3736d544d3eca4fb18e7
Jheimys Bassani <jheimysbb@gmail.com>
2022-09-09 14:38:13 -0300

    Estudando o SELECT

--- 4_MySQL/2_select/01-select.js
+++ 4_MySQL/2_select/01-select.js
@@ -1,2 +1,26 @@
 //Carrega o módulo do MySQL
-mysql = require('mysql')
+var mysql = require('mysql')
+
+//Cria a conexão
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'livro',
+    password:'livro123',
+    database:'livro'
+})
+
+//Conecta com BD
+connection.connect()
+
+//Cria uma consulta
+let sql = "select id,nome,tipo from carro";
+connection.query(sql, function(error, results, fields){
+    if(error) throw error
+    let carros = results
+    for(let i = 0; carros.length > i; i++){
+        console.log(carros[i].id + ":" + carros[i].nome)
+    }
+})
+
+//Fecha conexão
+connection.end()
